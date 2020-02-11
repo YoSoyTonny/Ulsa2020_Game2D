2f1cab4261e98af3ac0845ed7febefd038bfc6f4
YoSoyTonny <itonygame@gmail.com>
2020-02-11 16:41:40 -0700

    Actualizacion del juego

---
+++ Assets/Scripts/colectable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class colectable : MonoBehaviour
+{
+
+    Text txtScore;
+    [SerializeField]
+    int currentScore;
+
+    [SerializeField]
+    int points;
+
+    public int Points {get => points;}
+
+    void Awake()
+    {
+        txtScore = GetComponent<Text>();
+    }
+
+    public void AddPoints(int points)
+    {
+        currentScore += points;
+        txtScore.text = $"score: (currentScore) pts";
+    }
+}
---
+++ Assets/Scripts/flip/EPCFlip.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EPCFlip : IFlip
+{
+    public bool FlipSprite()
+    {
+        get => dir.x > 0 ? false : true;
+    }
+}
